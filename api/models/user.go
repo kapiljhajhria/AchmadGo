a89fc8e95e8267ceb8a756dd589a966147b983a1
samhj <sam4gentle@gmail.com>
2021-01-24 23:10:06 -0800

    added dob to model

--- api/models/user.go
+++ api/models/user.go
@@ -7,6 +7,7 @@ type User struct {
 	ProfileImage   string `bson:"profile_image,omitempty" json:"profile_image" xml:"profile_image" form:"profile_image"`
 	Email      string `bson:"email,omitempty" json:"email" xml:"email" form:"email"`
 	Gender      string `bson:"gender,omitempty" json:"gender" xml:"gender" form:"gender"`
+	DOB      string `bson:"dob,omitempty" json:"dob" xml:"dob" form:"dob"`
 	Country      string `bson:"country,omitempty" json:"country" xml:"country" form:"country"`
 	Password   string `bson:"password,omitempty" json:"password" xml:"password" form:"password"`
 	Status   string `bson:"status,omitempty" json:"status" xml:"status" form:"status"`
