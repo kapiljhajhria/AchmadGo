727f7295e4d7763607d29e511639a55c7bb619ce
samhj <sam4gentle@gmail.com>
2021-02-15 02:51:34 -0800

    updated blog model

--- api/models/blog.go
+++ api/models/blog.go
@@ -9,10 +9,11 @@ import (
 //Blog ...
 type Blog struct {
 	Title     string             `bson:"title,omitempty" json:"title" xml:"title" form:"title"`
+	Image     string             `bson:"image,omitempty" json:"image" xml:"image" form:"image"`
 	Directory string             `bson:"directory,omitempty" json:"directory" xml:"directory" form:"directory"`
 	Content   string             `bson:"content,omitempty" json:"content" xml:"content" form:"content"`
 	AuthorID  primitive.ObjectID `bson:"author_id,omitempty" json:"author_id" xml:"author_id" form:"author_id"`
 	Author    User               `bson:"author,omitempty" json:"author" xml:"author" form:"author"`
-	CreatedAt time.Time          `json:"created_at" bson:"created_at,omitempty"`
-	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at,omitempty"`
+	CreatedAt string          `json:"created_at" bson:"created_at,omitempty"`
+	UpdatedAt string          `json:"updated_at" bson:"updated_at,omitempty"`
 }
