71c88d8f5e98a72c9bbcfeea1c33988e23f37f1c
samhj <sam4gentle@gmail.com>
2021-02-16 23:47:51 -0800

    changed blog model

--- api/models/blog.go
+++ api/models/blog.go
@@ -13,8 +13,8 @@ type Blog struct {
 	AuthorID    primitive.ObjectID `bson:"author_id,omitempty" json:"author_id" xml:"author_id" form:"author_id"`
 	Image       string             `bson:"image,omitempty" json:"image" xml:"image" form:"image"`
 	Status      string             `bson:"status,omitempty" json:"status" xml:"status" form:"status"`
-	Likes       string             `bson:"likes,omitempty" json:"likes" xml:"likes" form:"likes"`
-	Views       string             `bson:"views,omitempty" json:"views" xml:"views" form:"views"`
+	Likes       []string             `bson:"likes,omitempty" json:"likes" xml:"likes" form:"likes"`
+	Views       []string             `bson:"views,omitempty" json:"views" xml:"views" form:"views"`
 	Author      User               `bson:"author,omitempty" json:"author" xml:"author" form:"author"`
 	PublishedAt string             `bson:"published_at,omitempty" json:"published_at" bson:"published_at,omitempty"`
 	LastUpdated string             `bson:"last_updated,omitempty" json:"last_updated" bson:"last_updated,omitempty"`
