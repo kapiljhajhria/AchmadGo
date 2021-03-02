c639b8e2c5968568749956679bde1b99ef0b728b
samhj <sam4gentle@gmail.com>
2021-03-02 11:36:10 -0800

    added magazine to site settings model

--- api/models/site_settings.go
+++ api/models/site_settings.go
@@ -1,8 +1,16 @@
 package models
 
+//Magazine ...
+type Magazine struct {
+	Title      string `bson:"title" json:"title" xml:"title" form:"title"`
+	Image   string `bson:"image" json:"image" xml:"image" form:"image"`
+	MagID string`bson:"magId" json:"magId" xml:"magId" form:"magId"`
+}
+
 //SiteSettings ...
 type SiteSettings struct {
 	MaxUploadSize string   `bson:"max_upload_size,omitempty" json:"max_upload_size" xml:"max_upload_size" form:"max_upload_size"`
 	AppStoreAppID string   `bson:"apple_store_app_id,omitempty" json:"apple_store_app_id" xml:"apple_store_app_id" form:"apple_store_app_id"`
 	Directories   []string `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
+	Magazines   Magazine `bson:"magazines,omitempty" json:"magazines" xml:"magazines" form:"magazines"`
 }
