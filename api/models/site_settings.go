058bb1ca42e86ef8b7d28cd3728d9a16a8293468
samhj <sam4gentle@gmail.com>
2021-03-03 08:30:58 -0800

    fixing errors

--- api/models/site_settings.go
+++ api/models/site_settings.go
@@ -2,9 +2,9 @@ package models
 
 //Magazine ...
 type Magazine struct {
-	Title      string `bson:"title" json:"title" xml:"title" form:"title"`
+	Title      string `bson:"title,omitempty" json:"title" xml:"title" form:"title"`
 	Image   string `bson:"image,omitempty" json:"image" xml:"image" form:"image"`
-	MagID string`bson:"magId" json:"magId" xml:"magId" form:"magId"`
+	MagID string`bson:"magId,omitempty" json:"magId" xml:"magId" form:"magId"`
 	Directories   []string `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
 	Author 	 []string `bson:"author,omitempty" json:"author" xml:"author" form:"author"`
 }
@@ -14,5 +14,5 @@ type SiteSettings struct {
 	MaxUploadSize string   `bson:"max_upload_size,omitempty" json:"max_upload_size" xml:"max_upload_size" form:"max_upload_size"`
 	AppStoreAppID string   `bson:"apple_store_app_id,omitempty" json:"apple_store_app_id" xml:"apple_store_app_id" form:"apple_store_app_id"`
 	Directories   []string `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
-	Magazines   []Magazine `bson:"magazines" json:"magazines" xml:"magazines" form:"magazines"`
+	Magazines   []Magazine `bson:"magazines,omitempty" json:"magazines" xml:"magazines" form:"magazines"`
 }
