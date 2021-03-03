ec07dce79a9efa86fa06062d701c2d0a0fcb262d
samhj <sam4gentle@gmail.com>
2021-03-02 22:18:34 -0800

    added author to sitemodel

--- api/models/site_settings.go
+++ api/models/site_settings.go
@@ -6,6 +6,7 @@ type Magazine struct {
 	Image   string `bson:"image,omitempty" json:"image" xml:"image" form:"image"`
 	MagID string`bson:"magId" json:"magId" xml:"magId" form:"magId"`
 	Directories   []string `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
+	Author 	 []string `bson:"author,omitempty" json:"author" xml:"author" form:"author"`
 }
 
 //SiteSettings ...
