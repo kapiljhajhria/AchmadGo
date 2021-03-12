c6238cf36d0c1904c75fa7c2ed20347621a6814a
samhj <sam4gentle@gmail.com>
2021-03-11 17:19:09 -0800

    changed directory model

--- api/models/site_settings.go
+++ api/models/site_settings.go
@@ -1,10 +1,16 @@
 package models
 
-//MagAuthor
+//MagAuthor ...
 type MagAuthor struct{
 	UserID string `bson:"user_id,omitempty" json:"user_id" xml:"user_id" form:"user_id"`
 	Status string `bson:"status,omitempty" json:"status" xml:"status" form:"status"`
 	Role string `bson:"role,omitempty" json:"role" xml:"role" form:"role"`
+}
+
+//Directory ...
+type Directory struct{
+	AuthorID string `bson:"author_id,omitempty" json:"author_id" xml:"author_id" form:"author_id"`
+	Directory string `bson:"directory,omitempty" json:"directory" xml:"directory" form:"directory"`
 }
 
 //Magazine ...
@@ -12,7 +18,7 @@ type Magazine struct {
 	Title      string `bson:"title,omitempty" json:"title" xml:"title" form:"title"`
 	Image   string `bson:"image,omitempty" json:"image" xml:"image" form:"image"`
 	MagID string`bson:"magId,omitempty" json:"magId" xml:"magId" form:"magId"`
-	Directories   []string `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
+	Directories   []Directory `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
 	Author 	 []MagAuthor `bson:"authors" json:"authors" xml:"authors" form:"authors"`
 }
 
@@ -20,6 +26,6 @@ type Magazine struct {
 type SiteSettings struct {
 	MaxUploadSize string   `bson:"max_upload_size,omitempty" json:"max_upload_size" xml:"max_upload_size" form:"max_upload_size"`
 	AppStoreAppID string   `bson:"apple_store_app_id,omitempty" json:"apple_store_app_id" xml:"apple_store_app_id" form:"apple_store_app_id"`
-	Directories   []string `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
+	Directories   []Directory `bson:"directories,omitempty" json:"directories" xml:"directories" form:"directories"`
 	Magazines   []Magazine `bson:"magazines" json:"magazines" xml:"magazines" form:"magazines"`
 }
