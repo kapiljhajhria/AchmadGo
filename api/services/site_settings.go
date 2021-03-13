6b99acaac61d6b52b466386fd92e79735d5eedb1
samhj <sam4gentle@gmail.com>
2021-03-12 18:33:01 -0800

    uppdate

--- api/services/site_settings.go
+++ api/services/site_settings.go
@@ -86,7 +86,7 @@ func UpdateSiteSettings(s *models.Server) error {
 
 				} else if updateData.Type == "add" {
 
-					update = bson.M{"$set":bson.M{"magazines":updateData.Magazine}}
+					update = bson.M{"$push":bson.M{"magazines": bson.M{"$set":&updateData.Magazine}}}
 
 				}
 			}
